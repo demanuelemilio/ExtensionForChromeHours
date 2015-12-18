936f255553608aad68c274cb016c876deb8d4403
Emilio De Manuel Aranda <demanuelemilio@gmail.com>
2015-12-18 09:35:52 +0100

    commit
    commit

--- js/app/routers/router.js
+++ js/app/routers/router.js
@@ -6,26 +6,24 @@ define([
       var router = Backbone.Router.extend({
 
 
-				routes:{
-					"":"index",
-			    "index":"index",
-			    "metricas":"metricas"
-
-				},
-				 initialize:function(view){
+		routes:{
+			"":"index",
+			"index":"index",
+			"metricas":"metricas"
+			},
+		initialize:function(view){
                    this.appView = view;
                  },
 				
-				index:function(view){
-                   this.appView.setPage('dash');
+		index:function(view){
+                   	this.appView.setPage('dash');
 			    	
-				},
+		},
 
-				metricas:function(){
-				   this.appView.setPage('metricas');				   				   
-				   this.appView.$("#content").append(this.appView.views['metricas'].render().el);
-
-				  }
+		metricas:function(){
+			this.appView.setPage('metricas');				   				   
+			this.appView.$("#content").append(this.appView.views['metricas'].render().el);
+		}
 
         });
    
